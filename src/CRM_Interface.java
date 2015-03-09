7ce845c53b124bb499687e9aee6424e0eea7f899
Markus Höcherl <mhoecherl@freenet.de>
2015-03-09 11:42:48 +0100

    CRM_Interface Kommentare

--- src/CRM_Interface.java
+++ src/CRM_Interface.java
@@ -2,7 +2,6 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
-import java.util.List;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -14,6 +13,10 @@ import java.util.List;
  *
  * @author MarkusH
  */
+
+//Security Manager
+//System.setSecurityManager(new RMISecurityManager());
+//Policy-File
 public interface CRM_Interface extends Remote {
     
     //neues Mitglied, das von Client geliefert wird, in Datenbank speichern
